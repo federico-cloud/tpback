76e243efcdedee8b227dede3123800ca0b1d9978
Federico Galan <fegv.2@hotmail.com.ar>
2023-06-18 00:35:58 -0300

    Se descomenta la propiedad domicilio

--- src/main/java/com/example/demo/dto/PacienteDTO.java
+++ src/main/java/com/example/demo/dto/PacienteDTO.java
@@ -1,17 +1,19 @@
 package com.example.demo.dto;
 
+import com.example.demo.persistence.entities.Domicilio;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
 import java.util.Date;
-
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PacienteDTO {
 
     //Attributes
     private String nombre;
     private String apellido;
-//    private Domicilio domicilio;
+    private String dni;
+    private Domicilio domicilio;
     private Date fechaRegistro;
-
 
 }
