31ef1ff6c5b57f835ec75559bc2bb67b6d93bc9c
Federico Galan <fegv.2@hotmail.com.ar>
2023-06-19 12:53:47 -0300

    Se modifica el atributo LocalDateTime por Date

--- src/main/java/com/example/demo/dto/TurnoDTO.java
+++ src/main/java/com/example/demo/dto/TurnoDTO.java
@@ -3,15 +3,14 @@ package com.example.demo.dto;
 import com.example.demo.persistence.entities.Odontologo;
 import com.example.demo.persistence.entities.Paciente;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.util.Date;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TurnoDTO {
     private Paciente paciente;
     private Odontologo odontologo;
-    private LocalDateTime fechaYHora;
+    private Date fechaYHora;
 }
