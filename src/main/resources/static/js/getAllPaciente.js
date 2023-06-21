b9b88a97260786936d592286674daa9dc5eb02c5
Octtag0 <octaviogonzalezmanucci@hotmail.com>
2023-06-21 09:05:23 -0300

    Se agregaron interfaces,estilos y js solo de agregar odontologo

---
+++ src/main/resources/static/js/getAllPaciente.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', function () {
+
+    const botonListar = document.querySelector('#listar');
+
+    botonListar.addEventListener('click', function (event) {
+
+        fetch('/odontologos/getAll')
+        .then(response => response.json())
+        .then(data => {
+            const odontologosTable = document.querySelector('#odontologos-table tbody');
+
+            // Vaciar la tabla antes de llenarla nuevamente
+            odontologosTable.innerHTML = '';
+
+            // Iterar sobre los datos recibidos y agregar las filas a la tabla
+            data.forEach(odontologo => {
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>${odontologo.id}</td>
+                            <td>${odontologo.nombre}</td>
+                            <td>${odontologo.apellido}</td>
+                            <td>${odontologo.matricula}</td>
+                            <td>${odontologo.turnos}</td>`;
+            odontologosTable.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error);
+        })
+    });
+
+});
