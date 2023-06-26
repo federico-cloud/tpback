fa914d61866f285db1fa1f43c0e1d7c2134a27f3
Federico Galan <fegv.2@hotmail.com.ar>
2023-06-25 21:32:54 -0300

    Se agrega la funcion para listar los turnos desde el front

---
+++ src/main/resources/static/js/turnos/listarTurnos.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', function () {
+
+    const botonListar = document.querySelector('#listar');
+    const turnosTable = document.querySelector('table#turnos-table tbody');
+
+    const obtenerTurnos = async() => {
+
+        const resp = await fetch('/turnos/getAll');
+        const data = await resp.json();
+
+        return data;
+
+    }
+
+    botonListar.addEventListener('click', async(event) => {
+
+        const data = await obtenerTurnos();
+        // Vaciar la tabla antes de llenarla nuevamente
+        turnosTable.innerHTML = '';
+
+        // Iterar sobre los datos recibidos y agregar las filas a la tabla
+        data.forEach(turno => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                    <td>${turno.id}</td>
+                    <td>${turno.paciente.nombre} ${turno.paciente.apellido}</td>
+                    <td>${turno.odontologo.nombre} ${turno.odontologo.apellido}</td>
+                    <td>${turno.fechaYHora}</td>
+                `
+            turnosTable.appendChild(row);
+        });
+    })
+});
