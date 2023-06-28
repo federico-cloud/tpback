ff4d6545bd9abad2d412c45e084fabd877f435d5
Federico Galan <fegv.2@hotmail.com.ar>
2023-06-28 01:17:21 -0300

    Se finaliza el modulo para modificar pacientes desde el frontend

--- src/main/resources/static/js/pacientes/modificarPaciente.js
+++ src/main/resources/static/js/pacientes/modificarPaciente.js
@@ -1,51 +1,75 @@
 window.addEventListener('load', function () {
-  const formulario = document.getElementById('modify-pacient');
 
-  formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
+  const formulario  = document.querySelector('#modify_pacient');
 
-    const id = document.getElementById('id').value;
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const dni = document.getElementById('dni').value;
-    const calle = document.getElementById('calle').value;
-    const numero = document.getElementById('numero').value;
-    const localidad = document.getElementById('localidad').value;
-    const provincia = document.getElementById('provincia').value;
+  const showAlert = (status) => {
 
-    const pacienteDTO = {
-      nombre,
-      apellido,
-      dni,
-      Domicilio: {
-        calle,
-        numero,
-        localidad,
-        provincia,
-      },
+    status === 200 ? (
+      Swal.fire({
+        icon: 'success',
+        title: 'Tarea completada',
+        text: 'El paciente fue modificado exitosamente.',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    ) : (
+      Swal.fire({
+        icon: 'error',
+        title: 'ERROR',
+        text: 'Hubo un error al modificar el paciente.',
+        showConfirmButton: false,
+        timer: 4000
+      })
+    );
+  }
+
+  const modificarPaciente = async (id) => {
+
+    const nombre    = formulario.querySelector('#nombre').value;
+    const apellido  = formulario.querySelector('#apellido').value;
+    const dni       = formulario.querySelector('#dni').value;
+    const calle     = formulario.querySelector('#calle').value;
+    const numero    = formulario.querySelector('#numero').value;
+    const localidad = formulario.querySelector('#localidad').value;
+    const provincia = formulario.querySelector('#provincia').value;
+
+    // Se crea el objeto paciente
+    const formData = {
+      'nombre': nombre,
+      'apellido': apellido,
+      'dni': dni,
+      domicilio: {
+        'calle': calle,
+        'numero': numero,
+        'localidad': localidad,
+        'provincia': provincia
+      }
     };
 
     const url = `/pacientes/modify/${id}`;
-
     const settings = {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(pacienteDTO),
+      body: JSON.stringify(formData)
     };
 
-    fetch(url, settings)
-        .then(response => response.json())
-        .then(data => {
-           document.getElementById('response').innerText = JSON.stringify(data);
-         })
-         .catch(error => {
-           console.error('Error:', error);
-         });
+    // Realiza la petición PUT a la API
+    const resp = await fetch(url, settings);
+    showAlert(resp.status);
 
-    formulario.reset();
+    return resp;
+  }
+
+  formulario.addEventListener('submit', async(event) => {
+
+    const id = formulario.querySelector('#id').value;
+    event.preventDefault();
+    modificarPaciente(id);
 
   });
+
 });
+
 
